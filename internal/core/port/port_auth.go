2bc2f993b4df5afef794b4acadf31ee7d628b840
fullgukbap <2hanjunbum6@gmail.com>
2024-09-03 15:03:36 +0900

    feat: token service 및 auth service 정의

---
+++ internal/core/port/port_auth.go
@@ -0,0 +1,22 @@
+package port
+
+import (
+	"context"
+
+	"toktok-backend-v1.0.1/internal/core/domain"
+)
+
+type TokenService interface {
+
+	// CreateToken 함수는 tokenPayload와 user값을 기반으로 새로운 토큰을 생성합니다.
+	CreateToken(tokenType domain.TokenType, user *domain.User) (string, error)
+
+	// VerifyToken 함수는 token을 검증하여 tokenPayload로 변환해줍니다.
+	VerifyToken(token string) (*domain.TokenPayload, error)
+}
+
+type AuthService interface {
+	Login(ctx context.Context, user *domain.User) (string, string, error)
+
+	Refresh(ctx context.Context, token string) (accessToken string, err error)
+}
